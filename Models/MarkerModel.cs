7ff3d43f1a490b6d24bfdc4ad035a792b4db7030
Maxim <maks.nasonov.03@gmail.com>
2025-05-18 22:04:52 +0700

    add deletePoint func

---
+++ Models/MarkerModel.cs
@@ -0,0 +1,8 @@
+namespace GeoCoder.Models;
+
+public class MarkerModel
+{
+	public string? Name { get; set; }
+	public float Latitude { get; set; }
+	public float Longitude { get; set; }
+}
--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,40 +1,65 @@
 let myMap;
 ymaps.ready(init);
-function init(){
-    // Создание карты.
+function init() {
     myMap = new ymaps.Map("map", {
-        // воспользуйтесь инструментом Определение координат.
         center: [55.76, 37.64],
         zoom: 7
     });
 }
 
 const geocodeForm = document.getElementById('geocodeForm')
-let points = document.getElementById('points')
+let pointsList = document.getElementById('points')
 geocodeForm.addEventListener('submit', handleFormSubmit)
+
+const markers = [];
+
+function deletePoint(index) {
+    myMap.geoObjects.remove(markers[index]);
+    markers.splice(index, 1);
+    rebuildPointsList();
+}
+
+function rebuildPointsList() {
+    const header = pointsList.querySelector('.disabled');
+    pointsList.innerHTML = '';
+    pointsList.appendChild(header);
+
+    markers.forEach((marker, index) => {
+        const name = marker.properties.get('name');
+        const coords = marker.geometry.getCoordinates();
+
+        const pointLi = document.createElement('li');
+        pointLi.className = 'list-group-item d-flex justify-content-between align-items-center';
+
+        pointLi.innerHTML = `
+            ${name} (${coords})
+            <button class="btn-close" aria-label="Удалить"></button>
+        `;
+
+        pointLi.querySelector('.btn-close').addEventListener('click', () => deletePoint(index));
+        pointsList.appendChild(pointLi);
+    });
+}
+
+function addPoint(point) {
+    const name = point.properties.get('name');
+    point.properties.set('iconContent', name);
+    const result = confirm(`Поставить точку "${name}"?`);
+
+    if (result) {
+        myMap.geoObjects.add(point);
+        markers.push(point);
+        rebuildPointsList();
+    }
+}
 
 function handleFormSubmit(event) {
     event.preventDefault()
     const formData = new FormData(geocodeForm);
-    let address = formData.get('address');
-    let myGeocoder = ymaps.geocode(address);
+    const address = formData.get('address');
+    const myGeocoder = ymaps.geocode(address);
     myGeocoder.then(function (res) {
-        let nearest = res.geoObjects.get(0);
-        let name = nearest.properties.get('name');
-        
-
-        result = confirm(`Поставить точку "${name}" ?`);
-        if (result) {
-            nearest.properties.set('iconContent', name);
-            nearest.options.set('preset', 'islands#redStretchyIcon');
-            myMap.geoObjects.add(nearest);
-            let li = document.createElement("li");
-            li.classList.add("list-group-item");
-            li.appendChild(document.createTextNode(`${name} (${nearest.geometry.getCoordinates()})`));
-            let button = document.createElement("button");
-            button.classList.add("btn-close");
-            li.appendChild(button);
-            points.appendChild(li);
-        }
-    });   
+        const nearest = res.geoObjects.get(0);
+        addPoint(nearest)
+    });
 }
